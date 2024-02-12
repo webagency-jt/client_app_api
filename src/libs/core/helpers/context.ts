a942c18341cba4f529cd51bdafc3d8a662ff3cb9
Mathys <mathystheolade973@gmail.com>
2024-02-12 23:01:28 +0100

    add todo

--- src/libs/core/helpers/context.ts
+++ src/libs/core/helpers/context.ts
@@ -1,5 +1,5 @@
 import { Context } from 'hono';
-
+// TODO: remove this
 export function isContextDefined(ctx: Context | undefined): void | Error {
   if (ctx) return;
   throw new Error('Context is not defined');
