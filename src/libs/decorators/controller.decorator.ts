a3b76b069da932413a664378ca895b1b3b0b2e1e
Mathys <mathystheolade973@gmail.com>
2024-02-23 08:30:58 +0100

    update type

--- src/libs/decorators/controller.decorator.ts
+++ src/libs/decorators/controller.decorator.ts
@@ -4,17 +4,12 @@ import { HttpErrors } from '@libs/errors/https-errors';
 import { User } from '@prisma/client';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { GuardAbstract } from '@libs/guards/guard.absract';
+import { RouteConfig } from '@libs/core/types/hono-zod.types';
 
 // Find a way to type the option to the following
-// <P extends string, R extends Omit<RouteConfig, 'path'> & {path: P;}>;
-// At the moment `Zod OpenApi Hono` don't export all type
 // TODO: add doc for option
 // TODO: dans le futur réecrire entièrement cette fonction pour la rendre plus propre
-export function Controller<T extends GuardAbstract>(options: {
-  method: 'post' | 'put' | 'get' | 'patch',
-  path: string,
-  responses: any,
-  request?: any,
+export function Controller<T extends GuardAbstract>(options: RouteConfig & {
   /** Define if the route need a specific permission to be accessed */
   guards?: T[],
   /** if an user try to an action that is for a different account then block it */
@@ -26,11 +21,12 @@ export function Controller<T extends GuardAbstract>(options: {
       // Access to property server from caller
       const server: App = Reflect.get(this, 'server');
       // Define route
-      const route = createRoute(options);
+      const { guards, secureRoute, ...routeMetadata } = options;
+      const route = createRoute(routeMetadata);
 
       return server.hono.openapi(route, async (ctx) => {
         // TODO: see if there is a better option than doing the following
-        if (options?.secureRoute) {
+        if (secureRoute) {
           const userJwt = ctx.get('jwtPayload') as User;
           let userId: string;
           if (route.path === 'get') {
@@ -44,8 +40,8 @@ export function Controller<T extends GuardAbstract>(options: {
             throw new HttpErrors(ReasonPhrases.UNAUTHORIZED, StatusCodes.UNAUTHORIZED);
           }
         }
-        if (options.guards) {
-          for (const guard of options.guards) {
+        if (guards) {
+          for (const guard of guards) {
             guard.run(ctx);
           }
         }
