de3ccee4e578b103e39a0a4e6a556d42468d8cf8
Mathys <mathystheolade973@gmail.com>
2024-02-26 21:17:49 +0100

    remove identifier

--- src/config/ioc/service-identifier.ts
+++ src/config/ioc/service-identifier.ts
@@ -1,17 +1,10 @@
 
 /**
+ * Will be removed for the time being but maybe will be used in the futur
  * Definition of types with symbols for dependency injection
  * @link https://www.npmjs.com/package/inversify
  */
-const SERVICE_IDENTIFIER = {
-  App: Symbol.for('App'),
-  Controller: Symbol.for('IController'),
-  Config: Symbol.for('Config'),
-  Logger: Symbol.for('AppLogger'),
-  Libs: Symbol.for('Libs'),
-  Guards: Symbol.for('Guards'),
-  Orm: Symbol.for('ORM'),
-};
+const SERVICE_IDENTIFIER = {};
 
 export { SERVICE_IDENTIFIER };
 
--- src/config/ioc/service-name.ts
+++ src/config/ioc/service-name.ts
@@ -1,35 +1,10 @@
 
 /**
+ * Will be removed for the time being but maybe will be used in the futur
  * Definition of named for dependency injection
  * @link https://github.com/inversify/InversifyJS/blob/master/wiki/named_bindings.md
  */
-const SERVICE_NAME = {
-  controllers: {
-    auth: 'auth',
-    notifications: 'notifications',
-    root: 'root',
-    user: 'user',
-    user_root: 'user_root',
-    user_settings: 'user_settings',
-    user_informations: 'user_informations',
-  },
-  libs: {
-    user_repository: 'user_repository',
-    user_settings_repository: 'user_settings_repository',
-    user_settings_service: 'user_settings_service',
-    user_informations_service: 'user_informations_service',
-    user_informations_repository: 'user_informations_repository',
-    user_service: 'user_service',
-    site_repository: 'site_repository',
-    site_service: 'site_service',
-  },
-  middleware: {
-    jwt_middleware: 'jwt_middleware',
-  },
-  guards: {
-    admin: 'admin',
-  },
-};
+const SERVICE_NAME = {};
 
 export { SERVICE_NAME };
 
